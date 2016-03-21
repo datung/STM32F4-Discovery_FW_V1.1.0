a22aba3fa1b1b2e771b8f50f66d5f7ae05cb4e3c
TungDang <anhtunghcmut08@gmail.com>
2016-03-21 15:03:56 +0700

    Initial commit
    Project includes many example for Discovery board from ST

---
+++ Project/Peripheral_Examples/PWR_STANDBY/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'PWR_STANDBY' 
+ * Target:  'PWR_STANDBY' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
